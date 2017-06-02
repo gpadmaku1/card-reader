db0398ec2846fa3c9872813945a9d78221e59887
Gautham Padmakumar <gpadmaku@gmail.com>
2017-06-01 21:59:57 -0400

    xml to java widget access completed

--- app/src/main/java/com/example/gautham/imagetotextanalysis/MainActivity.java
+++ app/src/main/java/com/example/gautham/imagetotextanalysis/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.gautham.imagetotextanalysis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get access to buttons and editTexts
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+
+        EditText nameText = (EditText) findViewById(R.id.nameText);
+        EditText phoneText = (EditText) findViewById(R.id.phoneText);
+        EditText emailText = (EditText) findViewById(R.id.emailText);
+
     }
 }
