b6cea0f0b8c9d16dd53cc5e6725c095e8d7defec
Gautham Padmakumar <gpadmaku@gmail.com>
2017-06-02 19:23:44 -0400

    Added camera intent and Google Vision API dependency

--- app/src/main/java/com/example/gautham/imagetotextanalysis/MainActivity.java
+++ app/src/main/java/com/example/gautham/imagetotextanalysis/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.gautham.imagetotextanalysis;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int IMAGE_CAPTURE_REQUEST = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
         EditText nameText = (EditText) findViewById(R.id.nameText);
         EditText phoneText = (EditText) findViewById(R.id.phoneText);
         EditText emailText = (EditText) findViewById(R.id.emailText);
+
+        cameraButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startCameraActivityIntent();
+            }
+        });
+    }
+
+    /**
+     * Starts the camera and
+     */
+    public void startCameraActivityIntent(){
+        Intent startCameraActivityIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(startCameraActivityIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(startCameraActivityIntent, IMAGE_CAPTURE_REQUEST);
+        }
 
     }
 }
