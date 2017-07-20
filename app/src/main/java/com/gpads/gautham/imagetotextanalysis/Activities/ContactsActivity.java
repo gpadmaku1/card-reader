60583ca70326fb1825b61179b3c7f6801e5798c7
Gautham Padmakumar <gpadmaku@gmail.com>
2017-07-19 23:29:42 -0400

    omg everything works!

--- app/src/main/java/com/gpads/gautham/imagetotextanalysis/Activities/ContactsActivity.java
+++ app/src/main/java/com/gpads/gautham/imagetotextanalysis/Activities/ContactsActivity.java
@@ -1,9 +1,13 @@
 package com.gpads.gautham.imagetotextanalysis.Activities;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.gpads.gautham.imagetotextanalysis.R;
 
@@ -13,6 +17,8 @@ public class ContactsActivity extends AppCompatActivity {
     private EditText nameText;
     private EditText emailText;
 
+    private Button contactsButton;
+
     private String name;
     private String email;
     private String phone;
@@ -21,15 +27,44 @@ public class ContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
-
         phoneText = (EditText) findViewById(R.id.phoneText);
         nameText = (EditText) findViewById(R.id.nameText);
         emailText = (EditText) findViewById(R.id.emailText);
-        phone = getIntent().getStringExtra("phoneNumber");
-        email = getIntent().getStringExtra("email");
-        name = getIntent().getStringExtra("name");
+        contactsButton = (Button) findViewById(R.id.contactBtn);
+
+        try {
+            phone = getIntent().getStringExtra("phoneNumber");
+            email = getIntent().getStringExtra("email");
+            name = getIntent().getStringExtra("name");
+
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            Toast.makeText(ContactsActivity.this, "Error", Toast.LENGTH_SHORT).show();
+        }
+
         phoneText.setText(phone);
         nameText.setText(name);
         emailText.setText(email);
+
+        contactsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startContacts();
+            }
+        });
     }
+
+
+    /**
+     * Creates a new intent to create and save a contact with the extracted info
+     */
+    private void startContacts() {
+        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
+        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, phone);
+        startActivity(intent);
+    }
+
 }
